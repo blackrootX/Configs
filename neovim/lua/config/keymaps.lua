5774eb9adc2cee7ca44af4eb05c4f919cf3d99b9
Songsheng Yang <songsheng.yang@cytel.com>
2025-07-03 04:07:45 +0800

    added init

--- neovim/lua/config/keymaps.lua
+++ neovim/lua/config/keymaps.lua
@@ -4,7 +4,5 @@
 vim.keymap.del("n", "<leader><leader>")
 vim.keymap.set("n", "<leader><leader>", function()
   require("flash").jump()
-end, { desc = "Flash jump" })
-
-
+end, { desc = "Flash jump" }) 
 vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
